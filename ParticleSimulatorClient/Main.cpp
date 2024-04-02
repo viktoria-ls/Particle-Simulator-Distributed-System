eb05f8bcd0c0b8e48709496d0337033f2000cb3c
Lila Sicuan <viktoria.sicuan@gmail.com>
2024-04-03 04:54:30 +0800

    Changed to translation approach

--- ParticleSimulatorClient/Main.cpp
+++ ParticleSimulatorClient/Main.cpp
@@ -3,41 +3,76 @@
 #include"imgui_impl_opengl3.h"
 
 #include<string>
+#include<vector>
+#include <algorithm>
 #include<iostream>
 #include<glad/glad.h>
 #include<GLFW/glfw3.h>
 
-struct User {
+struct Sprite {
 	double x = 400;
 	double y = 400;
-	double velocity = 10;
-
 };
 
 const int windowWidth = 800;
 const int windowHeight = 800;
-const int frameWidth = 1280;
-const int frameHeight = 720;
 
-User user;
+const double frameWidth = 1280;
+const double frameHeight = 720;
 
+const double spriteSize = std::min((frameWidth / 33.0), (frameHeight / 19.0));
+
+const int userVelocity = 10;
+Sprite user = {50, 50};
+
+// TODO: Send direction (and speed?) to server so it can 
 static void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods) {
 	if (key == GLFW_KEY_W && action == GLFW_PRESS) {
-		user.y -= user.velocity;
+		std::cout << "Pressed W";
+		user.y -= userVelocity;
 	}
 	else if (key == GLFW_KEY_A && action == GLFW_PRESS) {
-		user.x -= user.velocity;
+		std::cout << "Pressed A";
+		user.x -= userVelocity;
 	}
 	else if (key == GLFW_KEY_S && action == GLFW_PRESS) {
-		user.y += user.velocity;
+		std::cout << "Pressed S";
+		user.y += userVelocity;
 	}
 	else if (key == GLFW_KEY_D && action == GLFW_PRESS) {
-		user.x += user.velocity;
+		std::cout << "Pressed D";
+		user.x += userVelocity;
 	}
+}
+
+static void drawElements(std::vector<Sprite> &particles, float frameWindowWidth, float frameWindowHeight) {
+	ImDrawList* drawList = ImGui::GetWindowDrawList();
+
+	double clientUserX = frameWindowWidth + (frameWidth / 2);
+	double clientUserY = frameWindowHeight + (frameHeight / 2);
+	drawList->AddCircleFilled(ImVec2(clientUserX, clientUserY), spriteSize/2.0, IM_COL32_WHITE, 32);
+
+	/*double translatedPX = clientUserX + ((particles[0].x - user.x) * spriteSize / 2.0);
+	double translatedPY = clientUserY + ((particles[0].y - user.y) * spriteSize / 2.0);*/
+	double translatedPX = (clientUserX / user.x) * particles[0].x;
+	double translatedPY = (clientUserY / user.y) * particles[0].y;
+	drawList->AddCircleFilled(ImVec2(translatedPX, translatedPY), spriteSize/2.0, IM_COL32(160, 32, 240, 255), 32);
+
+	std::cout << "gorp\n";
+	std::cout << clientUserX << "\n";
+	std::cout << clientUserY << "\n";
+	std::cout << translatedPX << "\n";
+	std::cout << translatedPY << "\n";
+
 }
 
 int main()
 {
+	// dummy particle list
+	std::vector<Sprite> particles = {Sprite(), Sprite(), Sprite(), Sprite(), Sprite()};
+	particles[0].x = 70;
+	particles[0].y = 50;
+
 	// Initialize GLFW
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -61,6 +96,7 @@ int main()
 	gladLoadGL();
 	glViewport(0, 0, windowWidth, windowHeight);
 
+	// ImGui init
 	IMGUI_CHECKVERSION();
 	ImGui::CreateContext();
 	ImGuiIO& io = ImGui::GetIO(); (void)io;
@@ -73,20 +109,17 @@ int main()
 	// Main while loop
 	while (!glfwWindowShouldClose(window))
 	{
-		// Specify the color of the background
+		// Background color
 		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
-		// Clean the back buffer and assign the new color to it
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		ImGui_ImplOpenGL3_NewFrame();
 		ImGui_ImplGlfw_NewFrame();
 		ImGui::NewFrame();
 
-		// Calculate position for the center of the main window
+		// Calculate position for the center of the main window and imgui window
 		float mainWindowCenterX = ImGui::GetIO().DisplaySize.x / 2.0f;
 		float mainWindowCenterY = ImGui::GetIO().DisplaySize.y / 2.0f;
-
-		// Calculate position for the center of the window in the frame
 		float windowInFrameCenterX = mainWindowCenterX - (frameWidth / 2.0f);
 		float windowInFrameCenterY = mainWindowCenterY - (frameHeight / 2.0f);
 
@@ -95,13 +128,8 @@ int main()
 		ImGui::SetNextWindowSize(ImVec2(frameWidth, frameHeight));
 
 		ImGui::Begin("Title", NULL, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoTitleBar);
-
-		ImDrawList* drawList = ImGui::GetWindowDrawList();
-		drawList->AddCircleFilled(ImVec2(user.x, user.y), 4.5f, IM_COL32_WHITE, 8);
-
-		// Text that appears in the window
-		//ImGui::Text("Current FPS: %.3f", io.Framerate);
-		// Ends the window
+		ImGui::Text("Current FPS: %.3f", io.Framerate);
+		drawElements(particles, windowInFrameCenterX, windowInFrameCenterY);
 		ImGui::End();
 
 		// Renders the ImGUI elements
@@ -119,10 +147,7 @@ int main()
 	ImGui_ImplOpenGL3_Shutdown();
 	ImGui_ImplGlfw_Shutdown();
 	ImGui::DestroyContext();
-
-	// Delete window before ending the program
 	glfwDestroyWindow(window);
-	// Terminate GLFW before ending the program
 	glfwTerminate();
 	return 0;
 }
